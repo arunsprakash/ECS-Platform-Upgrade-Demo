5355fc696b97163c18f77f4771251ea9e7eb462e
ArunPrakash Sundararmaan <arun.prakash.s@spglobal.com>
2020-11-05 14:02:34 +0530

    Further changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@ComponentScan(
+	    basePackages = { "com.example.demo" })
+@Configuration
 @SpringBootApplication
 public class DemoApplication {
 
